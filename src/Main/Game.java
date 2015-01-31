58bc0c1c108f292a0c04a7300552d83448066754
Chan-Ho Kim <chanho.kim@outlook.com>
2015-01-30 18:06:44 -0600

    Creating Screen class

--- src/Main/Game.java
+++ src/Main/Game.java
@@ -1,13 +1,39 @@
 package Main;
 
-public class Game implements Runnable{
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
 
-	public static int width = 900;
-	public static int height = width / 16 * 9;
+import javax.swing.JFrame;
+
+import Main.graphics.Screen;
+
+public class Game extends Canvas implements Runnable{
+
+	private static final long serialVersionUID = 1L;
+	public static int width = 300;
+	public static int height = 200;
 	public static int scale = 3;
 	
 	private Thread thread;
+	private JFrame frame;
 	private boolean running = false;
+	
+	private Screen screen;
+	
+	private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+	private int[] pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
+	
+	public Game() {
+		Dimension size = new Dimension(width*scale,height*scale);
+		setPreferredSize(size);
+		screen = new Screen(width, height);
+		frame = new JFrame();
+	}
 	
 	public synchronized void start() {
 		thread = new Thread(this, "TeemoRun");
@@ -25,7 +51,47 @@ public class Game implements Runnable{
 
 	public void run() {
 		while(running) {
-			
+			update();
+			render();
 		}
+	}
+	
+	private void render() {
+		//smooth out the transition
+		BufferStrategy bs = getBufferStrategy();
+		if(bs == null){
+			createBufferStrategy(3);
+			return;
+		}
+		
+		screen.clear();
+		screen.render();
+		
+		for (int i = 0; i< pixels.length; i++) {
+			pixels[i] = screen.pixels[i];
+		}
+		
+		//swapping buffers = blitting
+		Graphics g = bs.getDrawGraphics();
+		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
+		g.dispose();
+		bs.show();
+	}
+
+	private void update() {
+		
+	}
+
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.frame.setResizable(false);
+		game.frame.setTitle("TeemoRun");
+		game.frame.add(game);
+		game.frame.pack();
+		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		game.frame.setLocationRelativeTo(null);
+		game.frame.setVisible(true);
+		
+		game.start();
 	}
 }
